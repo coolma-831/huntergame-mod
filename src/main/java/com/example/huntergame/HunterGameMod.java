14ad9552e1e54b9369bc8a477fde961fa6d0e0c6
coolma-831 <coolmamiku@163.com>
2025-06-01 03:32:39 +0800

    Create HunterGameMod.java

---
+++ src/main/java/com/example/huntergame/HunterGameMod.java
@@ -0,0 +1,10 @@
+package com.example.huntergame;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("huntergame")
+public class HunterGameMod {
+    public HunterGameMod() {
+        System.out.println("猎人游戏 Mod 已加载!");
+    }
+}
